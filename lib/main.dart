67cee9d2fa8a13645f351825d7aad6416ea5d2b2
ImrulEmon <emimrulemon@gmail.com>
2023-01-07 14:35:16 +0600

    Example 2 : StateNotifier

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_declarations, unnecessary_nullable_for_final_variable_declarations
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+void testIt() {
+  final int? int1 = 1;
+  final int? int2 = null;
+  final result = int1 + int2;
+  // ignore: avoid_print
+  print(result);
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Consumer(builder: (context, ref, child) {
+          final count = ref.watch(counterProvider);
+          final text = count == null ? 'Press the button' : count.toString();
+          return Text(text);
+        }),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextButton(
+            onPressed: ref.read(counterProvider.notifier).increment,
+            child: const Text('Increment Counter ++'),
+          ),
+        ],
+      ),
+    );
+  }
+}
